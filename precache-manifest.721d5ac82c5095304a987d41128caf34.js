de537ea7052870fa66a5e75b31a2ab1d30ec770f
Devendra Patidar <devendra.patidar@tokopedia.com>
2020-09-12 15:41:54 +0530

    adding manually bundle files

---
+++ precache-manifest.721d5ac82c5095304a987d41128caf34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90022705a62ee274e335abc91428b53",
+    "url": "/campaign-management/index.html"
+  },
+  {
+    "revision": "e6c246258ad329e2af90",
+    "url": "/campaign-management/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "064eb1584a2eed37d3fb",
+    "url": "/campaign-management/static/js/2.80f06504.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/campaign-management/static/js/2.80f06504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c246258ad329e2af90",
+    "url": "/campaign-management/static/js/main.76163441.chunk.js"
+  },
+  {
+    "revision": "bdfdad6f37c5351973f6",
+    "url": "/campaign-management/static/js/runtime-main.652c2f1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/campaign-management/static/media/logo.5d5d9eef.svg"
+  }
+]);
